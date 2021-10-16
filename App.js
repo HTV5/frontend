daab6e9761641088099cfa13f3af127bee08e4d6
iangu48 <iangu234@gmail.com>
2021-10-16 14:34:57 -0400

    add sql and meal planning

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './src/components/Home';
+import Home from './src/components/Home/Home';
+import DailyMeals from './src/components/DailyMeals/DailyMeals';
+import MealPlanning from './src/components/DailyMeals/MealPlanning';
+import { db } from './src/logic/db';
+
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  useEffect(() => {
+    // db.transaction((tx) => {
+    //   tx.executeSql(
+    //     "CREATE TABLE IF NOT EXISTS entries (id INTEGER PRIMARY KEY AUTOINCREMENT, photo TEXT, journal TEXT, emotions TEXT, date TEXT, timestamp DATETIME)"
+    //   );
+    // });
+  }, [])
   return <NavigationContainer>
-  <Stack.Navigator screenOptions={{headerShown: false}}>
-    <Stack.Screen name="Home" component={Home} />
-  </Stack.Navigator>
-</NavigationContainer>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="DailyMeals" component={DailyMeals} />
+      <Stack.Screen name="MealPlanning" component={MealPlanning} />
+    </Stack.Navigator>
+  </NavigationContainer>
 }
---
+++ src/components/DailyMeals/MealPlanning.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+
+export default function MealPlanning(props) {
+
+  const { day } = props.route.params
+  return <SafeAreaView>
+
+  </SafeAreaView>
+}
