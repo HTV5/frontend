64346e8e9ca41eeabd3c2cce6841fb7ff9209bc3
iangu48 <iangu234@gmail.com>
2021-10-16 11:40:41 -0400

    safeareaview

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView } from 'react-native';
 
 export default function Home(props) {
 
-    return <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-    </View>
+    return <SafeAreaView>
+        
+    </SafeAreaView>
 }
